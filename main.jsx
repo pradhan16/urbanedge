116ababac841f50d1c45a54d974bd8a6d6aed41d
pradhan16 <pradhanrupesh676@gmail.com>
2025-05-23 18:36:54 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UrbanEdge from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UrbanEdge />
+  </React.StrictMode>
+);
